b97db2443159af72165cffab31e9c8cdd5747a44
Dariusz Okonski <darek200180@gmail.com>
2020-02-25 14:32:44 +0100

    Updates

---
+++ precache-manifest.a50b7c7233cce0df3395e64d32ef829d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0766796b4a607f729887047861b9dc02",
+    "url": "/react-expenses-tracker/index.html"
+  },
+  {
+    "revision": "9e56d540d1c4a03e28dd",
+    "url": "/react-expenses-tracker/static/js/2.b66ffb3a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-expenses-tracker/static/js/2.b66ffb3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11ef2915ae654d2fdcf3",
+    "url": "/react-expenses-tracker/static/js/main.e43413ea.chunk.js"
+  },
+  {
+    "revision": "3a22b647c6eb56b4f5ff",
+    "url": "/react-expenses-tracker/static/js/runtime-main.df3aa748.js"
+  }
+]);
---
+++ static/js/main.e43413ea.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp021-expenses-tracker"]=this["webpackJsonp021-expenses-tracker"]||[]).push([[0],{3:function(e,n,t){e.exports=t(8)},8:function(e,n,t){"use strict";t.r(n);var r=t(0),a=t.n(r),s=t(2);var c=function(){return a.a.createElement("div",{className:"App"},a.a.createElement("h1",null,"Hello world"))};t.n(s).a.render(a.a.createElement(c,null),document.getElementById("root"))}},[[3,1,2]]]);
+//# sourceMappingURL=main.e43413ea.chunk.js.map
