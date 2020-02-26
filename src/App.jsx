3dee4df2913a298dbd48088a8db844d15da43819
Dariusz Okonski <darek200180@gmail.com>
2020-02-26 08:01:50 +0100

    add: temporary utilities components

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,20 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <h2 className="header">Hello world</h2>
+      <h2 className="header header--small">Header 1</h2>
+      <h2 className="header header--large">Header 2</h2>
+
+      <a className="btn btn--secondary" href="/">a tag button</a>
+      <button className="btn btn--primary">button tag button</button>
+      <br /><br />
+
+      <a className="btn btn--secondary btn--block" href="/">a tag button</a>
+      <button className="btn btn--primary btn--block">button tag button</button>
+
+      <input className="input" type="text" placeholder="enter name" />
+      <input className="input" type="number" placeholder="enter age" />
+
     </div>
   );
 }
