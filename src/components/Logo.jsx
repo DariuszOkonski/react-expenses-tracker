bfe52dc705d10d044dbbb7294d679d5224472036
Dariusz Okonski <darek200180@gmail.com>
2020-02-26 08:50:02 +0100

    add: Logo component

---
+++ src/components/Logo.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Logo = () => {
+  return (
+    <div id="logo">
+      <h3 className="logo__header">
+        Expenses Tracker
+      </h3>
+    </div>
+  );
+}
+
+export default Logo;
