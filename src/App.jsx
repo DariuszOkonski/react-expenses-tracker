15174744f06b1e587c466ffd50593236de246564
Dariusz Okonski <darek200180@gmail.com>
2020-02-26 08:52:16 +0100

    add: BrowserRouter to wrapp App and display Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,16 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <h2 className="header">Hello world</h2>
-      <h2 className="header header--small">Header 1</h2>
-      <h2 className="header header--large">Header 2</h2>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <div id="app">
+        <Header />
 
-      <a className="btn btn--secondary" href="/">a tag button</a>
-      <button className="btn btn--primary">button tag button</button>
-      <br /><br />
 
-      <a className="btn btn--secondary btn--block" href="/">a tag button</a>
-      <button className="btn btn--primary btn--block">button tag button</button>
-
-      <input className="input" type="text" placeholder="enter name" />
-      <input className="input" type="number" placeholder="enter age" />
-
-    </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
