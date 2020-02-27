7b2c4eacce0c562a3b12e7fc50b5897b4fa3e82c
Dariusz Okonski <darek200180@gmail.com>
2020-02-27 07:17:24 +0100

    add: Routing to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Header from './components/Header';
+import New from './views/New';
+import History from './views/History';
+import About from './views/About';
+import Error from './views/Error';
 
 function App() {
   return (
@@ -9,7 +13,12 @@ function App() {
         <Header />
 
         <div className="container">
-          Container
+          <Switch>
+            <Route exact path="/" component={New} />
+            <Route exact path="/history" component={History} />
+            <Route exact path="/about" component={About} />
+            <Route component={Error} />
+          </Switch>
         </div>
 
       </div>
