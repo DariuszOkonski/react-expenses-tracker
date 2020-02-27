1e2c7f7b964357abed7e547e8243e0097835626a
Dariusz Okonski <darek200180@gmail.com>
2020-02-27 07:36:10 +0100

    add: structure to About component

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -3,7 +3,19 @@ import React from 'react';
 const About = () => {
   return (
     <div id="about">
-      <h2>About</h2>
+      <h2 className="header header--large">About</h2>
+      <p className="paragraph">
+        Author: Dariusz Okonski
+      </p>
+      <p className="paragraph">
+        App Name: Expenses Tracker
+      </p>
+      <p className="paragraph">
+        Version: 1.0.0
+      </p>
+      <p className="paragraph">
+        Year: 2020
+      </p>
     </div>
   );
 }
