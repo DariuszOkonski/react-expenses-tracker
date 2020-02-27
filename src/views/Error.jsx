e3da33b4b541a90faac92537bb3411315ce40316
Dariusz Okonski <darek200180@gmail.com>
2020-02-27 07:30:34 +0100

    add: structure to Error component

--- src/views/Error.jsx
+++ src/views/Error.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 const Error = () => {
   return (
     <div id="error">
-      <h2>Error</h2>
+      <h2 className="header header--large">Error</h2>
+      <p className="paragraph">
+        There is no such a route!!! Choose one from the Navigation above.
+      </p>
     </div>
   );
 }
