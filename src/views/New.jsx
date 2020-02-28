b9436915ff015224230a22cb0cd3062de1ea4b54
Dariusz Okonski <darek200180@gmail.com>
2020-02-28 09:12:15 +0100

    change: display Income and Expense and wrap it into .costs

--- src/views/New.jsx
+++ src/views/New.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Balance from '../components/Balance';
+import Income from '../components/Income';
+import Expense from '../components/Expense';
 
 const New = () => {
   return (
@@ -7,6 +9,12 @@ const New = () => {
       <h2 className="header">New Transactions</h2>
 
       <Balance />
+
+      <div className="costs">
+        <Income />
+        <Expense />
+      </div>
+
     </div>
   );
 }
