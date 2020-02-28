380fe032b5da0a0cfa355d629e41b5f00ea079c1
Dariusz Okonski <darek200180@gmail.com>
2020-02-28 08:40:05 +0100

    add: displayBudget, displayIncome, displayExpenses

--- src/contexts/BudgetContext.jsx
+++ src/contexts/BudgetContext.jsx
@@ -10,10 +10,24 @@ const BudgetContextProvider = (props) => {
     { id: 4, text: 'book', income: 0, expense: 23 },
     { id: 5, text: 'salary', income: 320, expense: 0 },
     { id: 6, text: 'desk lamp', income: 0, expense: 98 },
-  ])
+  ]);
+
+  const displayBudget = () => {
+    const income = budget.reduce((acc, el) => acc + el.income, 0);
+    const expense = budget.reduce((acc, el) => acc + el.expense, 0);
+    return income - expense;
+  }
+
+  const displayIncome = () => {
+    return budget.reduce((acc, el) => acc + el.income, 0);
+  }
+
+  const displayExpenses = () => {
+    return budget.reduce((acc, el) => acc + el.expense, 0);
+  }
 
   return (
-    <BudgetContext.Provider value={{ budget }}>
+    <BudgetContext.Provider value={{ budget, displayBudget, displayIncome, displayExpenses }}>
       {props.children}
     </BudgetContext.Provider>
   );
