0661be0afbedea66d16fec4a0ab3159b70be37fd
Dariusz Okonski <darek200180@gmail.com>
2020-02-28 08:03:39 +0100

    add: components Balance, Expense, Income, Transactions

---
+++ src/components/Balance.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Balance = () => {
+  return (
+    <div id="balance">
+      <p className="paragraph">YOUR BALANCE:</p>
+      <p className="paragraph">$ 420.00</p>
+    </div>
+  );
+}
+
+export default Balance;
