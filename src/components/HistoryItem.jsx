080524e08e63c739e38121e284a90b805707b6a8
Dariusz Okonski <darek200180@gmail.com>
2020-02-28 07:39:56 +0100

    change: HistoryItem to display all given data

--- src/components/HistoryItem.jsx
+++ src/components/HistoryItem.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
 
-const HistoryItem = () => {
+const HistoryItem = ({ id, text, income, expense }) => {
   return (
     <div id="history-item">
       <button className="btn btn--secondary">x</button>
-      <p className="item-paragraph">Salary</p>
+      <p className="item-paragraph">{text}</p>
       <p className="item-paragraph">
-        <span>+</span>$ 122220
+        {
+          income > 0 ?
+            <span className="green-text"><span>+ $</span>{income}</span>
+            :
+            <span className="red-text"><span>- $</span>{expense}</span>
+        }
+
       </p>
-      <div className="green"></div>
+      {
+        income > 0 ?
+          <div className="green"></div>
+          :
+          <div className="red"></div>
+      }
+
     </div>
   );
 }
