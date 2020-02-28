d5302af91823393f1b0875958c172373f9d316dc
Dariusz Okonski <darek200180@gmail.com>
2020-02-28 07:41:39 +0100

    change: wrap .container with BudgetContextProvider

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import New from './views/New';
 import History from './views/History';
 import About from './views/About';
 import Error from './views/Error';
+import BudgetContextProvider from './contexts/BudgetContext';
 
 function App() {
   return (
@@ -12,14 +13,16 @@ function App() {
       <div id="app">
         <Header />
 
-        <div className="container">
-          <Switch>
-            <Route exact path="/" component={New} />
-            <Route exact path="/history" component={History} />
-            <Route exact path="/about" component={About} />
-            <Route component={Error} />
-          </Switch>
-        </div>
+        <BudgetContextProvider>
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={New} />
+              <Route exact path="/history" component={History} />
+              <Route exact path="/about" component={About} />
+              <Route component={Error} />
+            </Switch>
+          </div>
+        </BudgetContextProvider>
 
       </div>
     </BrowserRouter>
