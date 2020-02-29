710809755d80fd7289ef172312f784ea07854f1e
Dariusz Okonski <darek200180@gmail.com>
2020-02-29 10:12:45 +0100

    add: logic is budget is empty

--- src/components/HistoryItems.jsx
+++ src/components/HistoryItems.jsx
@@ -6,14 +6,20 @@ import { BudgetContext } from '../contexts/BudgetContext';
 const HistoryItems = () => {
   const { budget } = useContext(BudgetContext);
 
-  return (
+  return budget.length > 0 ? (
     <div id="history-items">
       {
         budget.map(element =>
           <HistoryItem key={element.id} {...element} />)
       }
     </div>
-  );
+  ) : (
+      <div id="history-items">
+        <p className="paragraph">
+          Nothing to display
+      </p>
+      </div>
+    );
 }
 
 export default HistoryItems;
