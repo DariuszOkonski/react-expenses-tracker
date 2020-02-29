509c0bdf940365714b6a0d3c0208ee51232cd309
Dariusz Okonski <darek200180@gmail.com>
2020-02-29 09:57:04 +0100

    add: localStorage logic

--- src/contexts/BudgetContext.jsx
+++ src/contexts/BudgetContext.jsx
@@ -1,16 +1,23 @@
 import React, { createContext, useState } from 'react';
+import uuid from 'uuid/v1';
+import { useEffect } from 'react';
 
 export const BudgetContext = createContext();
 
 const BudgetContextProvider = (props) => {
-  const [budget, setBudget] = useState([
-    { id: 1, text: 'salary', income: 500, expense: 0 },
-    { id: 2, text: 'book', income: 0, expense: 10 },
-    { id: 3, text: 'monitor', income: 0, expense: 180 },
-    { id: 4, text: 'book', income: 0, expense: 23 },
-    { id: 5, text: 'salary', income: 320, expense: 0 },
-    { id: 6, text: 'desk lamp', income: 0, expense: 98 },
-  ]);
+  const [budget, setBudget] = useState([]);
+
+  // useEffect(() => {
+  //   localStorage.setItem('budget', JSON.stringify(budget));
+  //   console.log('useEffect1');
+  // }, [budget]);
+
+  useEffect(() => {
+    let localData = localStorage.getItem('budget');
+    localData = localData === null ? [] : JSON.parse(localData);
+    // console.log(localData);
+    setBudget(localData);
+  }, [])
 
   const displayBudget = () => {
     const income = budget.reduce((acc, el) => acc + el.income, 0);
@@ -26,9 +33,21 @@ const BudgetContextProvider = (props) => {
     return budget.reduce((acc, el) => acc + el.expense, 0);
   }
 
+  const addItem = (text, income, expense) => {
+    let newBudget = [...budget, { id: uuid(), text, income: Number(income), expense: Number(expense) }];
+    setBudget(newBudget);
+    localStorage.setItem('budget', JSON.stringify(newBudget));
+  }
+
+  const removeItem = (id) => {
+    let newBudget = budget.filter(item => item.id !== id);
+    setBudget(newBudget);
+    localStorage.setItem('budget', JSON.stringify(newBudget));
+  }
+
   return (
     <BudgetContext.Provider
-      value={{ budget, displayBudget, displayIncome, displayExpenses }
+      value={{ budget, displayBudget, displayIncome, displayExpenses, addItem, removeItem }
       }>
       {props.children}
     </BudgetContext.Provider>
