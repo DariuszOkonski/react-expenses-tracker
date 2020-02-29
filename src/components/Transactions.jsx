1eb4c81f3c1b62b4c03ea06c49f5a7193b98875d
Dariusz Okonski <darek200180@gmail.com>
2020-02-29 09:56:32 +0100

    add: logic for Transactions

--- src/components/Transactions.jsx
+++ src/components/Transactions.jsx
@@ -1,15 +1,54 @@
 import React from 'react';
+import { useContext } from 'react';
+import { BudgetContext } from '../contexts/BudgetContext';
+import { useState } from 'react';
 
 const Transactions = () => {
+  const { addItem } = useContext(BudgetContext);
+  const [text, setText] = useState('');
+  const [amount, setAmount] = useState('');
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addItem(text, amount, 0)
+    clearInputs();
+  }
+
+  const handleExpense = (e) => {
+    e.preventDefault();
+    addItem(text, 0, amount);
+    clearInputs();
+  }
+
+  const clearInputs = () => {
+    setText('')
+    setAmount('')
+  }
+
   return (
     <div id="transactions">
       <p className="paragraph">ADD NEW TRANSACTION</p>
-      <form>
-        <input type="text" placeholder="enter text..." required />
-        <input type="number" placeholder="amount..." required />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="enter text..."
+          required
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+
+        <input
+          type="number"
+          placeholder="amount..."
+          required
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+        />
+
         <div className="buttons-section">
-          <input type="submit" className="btn btn--primary" value="Add Income" />
-          <input type="submit" className="btn btn--secondary" value="Add Expense" />
+          <button type="submit" className="btn btn--primary">Add Income</button>
+          <button onClick={handleExpense} className="btn btn--secondary">Add Expense</button>
         </div>
       </form>
     </div>
