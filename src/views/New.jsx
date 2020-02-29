f881c8b973b5707c8d6786da615dda149bdac512
Dariusz Okonski <darek200180@gmail.com>
2020-02-29 08:23:29 +0100

    change: display Transactions at New component

--- src/views/New.jsx
+++ src/views/New.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Balance from '../components/Balance';
 import Income from '../components/Income';
 import Expense from '../components/Expense';
+import Transactions from '../components/Transactions';
 
 const New = () => {
   return (
@@ -14,6 +15,8 @@ const New = () => {
         <Income />
         <Expense />
       </div>
+
+      <Transactions />
 
     </div>
   );
